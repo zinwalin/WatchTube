d680443ea221ec5836a392af539f9d6984f35ac3
llsc12 <42747613+llsc12@users.noreply.github.com>
2022-02-18 22:15:50 +0000

    idk

--- WatchTube/ContentView.swift
+++ WatchTube/ContentView.swift
@@ -8,9 +8,19 @@
 import SwiftUI
 
 struct ContentView: View {
+    @Environment(\.colorScheme) var colorScheme
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            Image(colorScheme == .dark ? "iconDark" : "iconLight")
+                .resizable()
+                .frame(width: 200.0, height: 200.0)
+                .scaledToFit()
+                .clipShape(Circle())
+            Text("Under Construction!")
+                .font(Font.system(size:30))
+                .padding(.top, 5.0)
+        }
     }
 }
 
