5c90de53453441810b4f233bfbfe8f701b20a20f
llsc12 <42747613+llsc12@users.noreply.github.com>
2022-02-20 17:16:33 +0000

    search ui and working youtubekit implementation

---
+++ WatchTube WatchKit Extension/Scenes/Home/SearchInterfaceController.swift
@@ -0,0 +1,94 @@
+//
+//  SearchInterfaceController.swift
+//  WatchTube WatchKit Extension
+//
+//  Created by llsc12 on 20/02/2022.
+//
+
+import WatchKit
+import Foundation
+import Alamofire
+
+class SearchInterfaceController: WKInterfaceController {
+    @IBOutlet weak var searchTermsTableRow: WKInterfaceTable!
+    @IBOutlet weak var tableLabel: WKInterfaceLabel!
+    @IBOutlet weak var input: WKInterfaceTextField!
+    @IBOutlet weak var clearButton: WKInterfaceButton!
+    var tableContents: Array<String> = []
+    
+    override func awake(withContext context: Any?) {
+        super.awake(withContext: context)
+        input.setRelativeWidth(1, withAdjustment: 0)
+        updateSearch(terms: "")
+    }
+
+    @IBAction func ClearField() {
+        input.setText("")
+        input.setRelativeWidth(1, withAdjustment: 0)
+        updateSearch(terms: "")
+    }
+    @IBAction func SearchFieldEdited(_ value: NSString?) {
+        let terms = value as String? ?? ""
+        updateSearch(terms: terms)
+        if terms != "" {
+            input.setRelativeWidth(0.9, withAdjustment: 0)
+        } else {
+            input.setRelativeWidth(1, withAdjustment: 0)
+        }
+    }
+
+    func updateSearch(terms: String) {
+        if terms == "" {
+            self.tableLabel.setText("Recents")
+            // show the recent searches
+            let keywordsHistory = UserDefaults.standard.stringArray(forKey: preferencesKeys.keywordsHistory) ?? [String]()
+            let lastTwentyKeywordsHistory = Array(keywordsHistory.suffix(20))
+            tableContents = []
+            searchTermsTableRow.setNumberOfRows(lastTwentyKeywordsHistory.count, withRowType: "SearchTermsRow")
+            for i in 0 ..< lastTwentyKeywordsHistory.count {
+                guard let row = searchTermsTableRow.rowController(at: i) as? SearchTermsRow else {
+                    continue
+                }
+                row.label.setText(lastTwentyKeywordsHistory[i])
+                row.text = lastTwentyKeywordsHistory[i]
+                tableContents.append(lastTwentyKeywordsHistory[i])
+            }
+        } else {
+            let suggestionpath = "https://\(UserDefaults.standard.string(forKey: settingsKeys.instanceUrl) ?? Constants.defaultInstance)/api/v1/search/suggestions?q=\(terms.addingPercentEncoding(withAllowedCharacters: .urlHostAllowed)!)"
+            AF.request(suggestionpath) { $0.timeoutInterval = 3 }.validate().responseJSON {res in
+                self.tableLabel.setText("Suggestions")
+                switch res.result {
+                case .success(let data):
+                    let json = data as! Dictionary<String, Any>
+                    if json["error"] as? String != nil {print("api is having issues")}
+                    let suggestions = json["suggestions"] as! Array<String>
+                    
+                    self.searchTermsTableRow.setNumberOfRows(suggestions.count, withRowType: "SearchTermsRow")
+                    for i in 0 ..< suggestions.count {
+                        guard let row = self.searchTermsTableRow.rowController(at: i) as? SearchTermsRow else {
+                            continue
+                        }
+                        row.label.setText(suggestions[i])
+                        row.text = suggestions[i]
+                        self.tableContents.append(suggestions[i])
+                    }
+                case .failure(_):
+                    self.searchTermsTableRow.setNumberOfRows(1, withRowType: "SearchTermsRow")
+                    for i in 0 ..< 1 {
+                        guard let row = self.searchTermsTableRow.rowController(at: i) as? SearchTermsRow else {
+                            continue
+                        }
+                        row.label.setText(terms)
+                        row.text = terms
+                        self.tableContents.append(terms)
+                    }
+                }
+            }
+        }
+    }
+    
+    override func table(_ table: WKInterfaceTable, didSelectRowAt rowIndex: Int) {
+        let term = tableContents[rowIndex]
+        self.pushController(withName: "VideoListInterfaceController", context: term)
+    }
+}
--- WatchTube WatchKit Extension/Scenes/Players/Preload/NowPlayingInterfaceContoller.swift
+++ WatchTube WatchKit Extension/Scenes/Players/Preload/NowPlayingInterfaceContoller.swift
@@ -9,6 +9,7 @@ import Foundation
 import WatchKit
 import Alamofire
 import SDWebImage
+import YouTubeKit
 
 var video: Video!
 
@@ -42,13 +43,32 @@ class NowPlayingInterfaceController: WKInterfaceController {
     }
     
     override func awake(withContext context: Any?) {
+        Task {
+            await main(context: context)
+        }
+    }
+    
+    @IBAction func pushToMovie() {
+        pushController(withName: "HlsPlayer", context: "")
+    }
+    
+    func showMovieFade(movie: WKInterfaceButton!) {
+        movie.setAlpha(0)
+        animate(withDuration: 0.5) {
+            movie.setHidden(false)
+            movie.setAlpha(0.5)
+        }
+    }
+    
+    func main(context: Any?) async {
         if context != nil {
             video = context as? Video
         }
         
-        //cache metadata
+        // cache metadata
         meta.cacheVideoInfo(id: video.id)
         meta.cacheChannelInfo(udid: meta.getVideoInfo(id: video.id, key: "channelId") as! String)
+        // video meta should already be cached. if not, everything will probably break.
         
         progressBar.setHidden(true)
         progressBar.setRelativeWidth(0.0001, withAdjustment: 0)
@@ -76,122 +96,158 @@ class NowPlayingInterfaceController: WKInterfaceController {
         }
         
         super.awake(withContext: context)
-        
-        let dataPath = "https://\(UserDefaults.standard.string(forKey: settingsKeys.instanceUrl) ?? Constants.defaultInstance)/api/v1/videos/\(video.id)?fields=formatStreams(url,container),adaptiveFormats(url,container,encoding,bitrate)"
-        
-        self.statusLabel.setText("Downloading data...")
+                
+        self.statusLabel.setText("Getting stream data...")
         self.movieLoading.setImageNamed("loading")
         self.movieLoading.startAnimatingWithImages(in: NSRange(location: 0, length: 6), duration: 0.75, repeatCount: 0)
         self.movie.setHidden(true)
         progressBar.setHidden(false)
         
-        AF.request(dataPath).responseJSON { res in
-            switch res.result {
-            case .success(let data):
-                let videoDetails = data as! Dictionary<String, Any>
-                self.statusLabel.setText("Parsing data...")
-                self.progressBar.setRelativeWidth(0.35, withAdjustment: 0)
-
-                // Required variables in this scope
-                // - streamUrl (to set)
-                // - fileType (to set)
-                // - dlType (to use)
-                
-                // parse the video info for links. check dl type to set streamUrl to src of audio or video
-                
-                if (videoDetails["adaptiveFormats"] as! Array<Dictionary<String, Any>>).count == 0 || (videoDetails["formatStreams"] as! Array<Dictionary<String, Any>>).count == 0 {
-                    self.statusLabel.setText("No streams found")
-                    self.movieLoading.stopAnimating()
-                    self.movieLoading.setImageNamed("error")
-                    self.progressBar.setHidden(true)
-                    break
+        do {
+            // get streams
+            let ytVideo = YouTube(videoID: video.id)
+            let streams = try await ytVideo.streams
+            var stream: YouTubeKit.Stream!
+            
+            if dlType == "video" {
+                let videoStreams = streams.filter { $0.isProgressive && $0.subtype == "mp4" }
+                if quality == "hd" {
+                    stream = videoStreams.highestResolutionStream()
+                } else if quality == "sd" {
+                    stream = videoStreams.lowestResolutionStream()
                 }
-                                    
-                if dlType == "video" {
-                    let formatStreams = videoDetails["formatStreams"] as! Array<Dictionary<String, Any>>
-                    if self.quality == "hd" {
-                        let streamData = formatStreams[formatStreams.count - 1]
-                        self.streamUrl = streamData["url"] as! String
-                        self.fileType = "mp4"
-                    } else if self.quality == "sd" {
-                        var streamData: Dictionary<String,String>
-                        if formatStreams.count >= 2 {
-                            streamData = formatStreams[1] as! Dictionary<String,String>
-                        } else {
-                            streamData = formatStreams[0] as! Dictionary<String,String>
-                        }
-                        self.streamUrl = streamData["url"]!
-                        self.fileType = "mp4"
-                    }
-                } else if dlType == "audio" {
-                    let adaptiveFormats = videoDetails["adaptiveFormats"] as! Array<Dictionary<String, Any>>
-                    var aacFormats: Array<Dictionary<String, Any>> = []
-                    for item in adaptiveFormats {
-                        if item["encoding"] != nil {
-                            if item["encoding"] as! String == "aac" {
-                                aacFormats.append(item)
-                            }
-                        }
-                    }
-                    
-                    var highestBitrate: Int = 0
-                    if self.quality == "sd" {highestBitrate = Int.max}
-                    var format: Dictionary<String, Any> = [:]
-                    if aacFormats.count != 1 {
-                        for item in aacFormats {
-                            if self.quality == "hd" {
-                                if (item["bitrate"] as! NSString).integerValue > highestBitrate {
-                                    highestBitrate = (item["bitrate"] as! NSString).integerValue
-                                    format = item
-                                }
-                            } else if self.quality == "sd" {
-                                if (item["bitrate"] as! NSString).integerValue < highestBitrate {
-                                    highestBitrate = (item["bitrate"] as! NSString).integerValue
-                                    format = item
-                                }
-                            }
-                        }
-                    } else {format = aacFormats[0]}
-                    self.streamUrl = format["url"] as! String
-                    self.fileType = "m4a"
+            } else if dlType == "audio" {
+                let audioStreams = streams.filterAudioOnly()
+                if quality == "hd" {
+                    stream = audioStreams.filter { $0.subtype == "mp4" }
+                    .highestAudioBitrateStream()
+                } else if quality == "sd" {
+                    stream = audioStreams.filter { $0.subtype == "mp4" }
+                    .lowestAudioBitrateStream()
                 }
-                
-                if UserDefaults.standard.bool(forKey: settingsKeys.proxyContent) {
-                    // modify streamURL to use instance proxying
-                    let host = (URL(string: self.streamUrl)?.host)! as String
-                    self.streamUrl = self.streamUrl.replacingOccurrences(of: host, with: UserDefaults.standard.string(forKey: settingsKeys.instanceUrl) ?? Constants.defaultInstance)
-                }
-                        
-                self.progressBar.setRelativeWidth(0.65, withAdjustment: 0)
-                UserDefaults.standard.set(self.streamUrl, forKey: hls.url)
-                
-                self.statusLabel.setText("Ready.")
-                self.showMovieFade(movie: self.movie)
-                self.movieLoading.setHidden(true)
-                self.movieLoading.stopAnimating()
-                self.progressBar.setHidden(true)
-                
-            case .failure(_):
+            }
+            streamUrl = stream.url.absoluteString
+            if streamUrl == "" {
                 self.statusLabel.setText("Error getting data")
                 self.movieLoading.stopAnimating()
                 self.movieLoading.setImageNamed("error")
                 self.progressBar.setHidden(true)
+                return
             }
-        }.downloadProgress { progress in
-            self.progressBar.setRelativeWidth(progress.fractionCompleted, withAdjustment: 0)
+            
+            // start finishing up
+            
+            UserDefaults.standard.set(streamUrl, forKey: hls.url)
+
+            statusLabel.setText("Ready.")
+            showMovieFade(movie: movie)
+            movieLoading.setHidden(true)
+            movieLoading.stopAnimating()
+            progressBar.setHidden(true)
+            
+        } catch {
+            self.statusLabel.setText("Error getting data")
+            self.movieLoading.stopAnimating()
+            self.movieLoading.setImageNamed("error")
+            self.progressBar.setHidden(true)
         }
-    }
-    
-    @IBAction func pushToMovie() {
-        pushController(withName: "HlsPlayer", context: "")
-    }
-    
-    func showMovieFade(movie: WKInterfaceButton!) {
-        movie.setAlpha(0)
-        animate(withDuration: 0.5) {
-            movie.setHidden(false)
-            movie.setAlpha(0.5)
-        }
+            /// Old code here bc why not yk
+//        let dataPath = "https://\(UserDefaults.standard.string(forKey: settingsKeys.instanceUrl) ?? Constants.defaultInstance)/api/v1/videos/\(video.id)?fields=formatStreams(url,container),adaptiveFormats(url,container,encoding,bitrate)"
+//        AF.request(dataPath).responseJSON { res in
+//            switch res.result {
+//            case .success(let data):
+//                let videoDetails = data as! Dictionary<String, Any>
+//                self.statusLabel.setText("Parsing data...")
+//                self.progressBar.setRelativeWidth(0.35, withAdjustment: 0)
+//
+//                // Required variables in this scope
+//                // - streamUrl (to set)
+//                // - fileType (to set)
+//                // - dlType (to use)
+//
+//                // parse the video info for links. check dl type to set streamUrl to src of audio or video
+//
+//                if (videoDetails["adaptiveFormats"] as! Array<Dictionary<String, Any>>).count == 0 || (videoDetails["formatStreams"] as! Array<Dictionary<String, Any>>).count == 0 {
+//                    self.statusLabel.setText("No streams found")
+//                    self.movieLoading.stopAnimating()
+//                    self.movieLoading.setImageNamed("error")
+//                    self.progressBar.setHidden(true)
+//                    break
+//                }
+//
+//                if dlType == "video" {
+//                    let formatStreams = videoDetails["formatStreams"] as! Array<Dictionary<String, Any>>
+//                    if self.quality == "hd" {
+//                        let streamData = formatStreams[formatStreams.count - 1]
+//                        self.streamUrl = streamData["url"] as! String
+//                        self.fileType = "mp4"
+//                    } else if self.quality == "sd" {
+//                        var streamData: Dictionary<String,String>
+//                        if formatStreams.count >= 2 {
+//                            streamData = formatStreams[1] as! Dictionary<String,String>
+//                        } else {
+//                            streamData = formatStreams[0] as! Dictionary<String,String>
+//                        }
+//                        self.streamUrl = streamData["url"]!
+//                        self.fileType = "mp4"
+//                    }
+//                } else if dlType == "audio" {
+//                    let adaptiveFormats = videoDetails["adaptiveFormats"] as! Array<Dictionary<String, Any>>
+//                    var aacFormats: Array<Dictionary<String, Any>> = []
+//                    for item in adaptiveFormats {
+//                        if item["encoding"] != nil {
+//                            if item["encoding"] as! String == "aac" {
+//                                aacFormats.append(item)
+//                            }
+//                        }
+//                    }
+//
+//                    var highestBitrate: Int = 0
+//                    if self.quality == "sd" {highestBitrate = Int.max}
+//                    var format: Dictionary<String, Any> = [:]
+//                    if aacFormats.count != 1 {
+//                        for item in aacFormats {
+//                            if self.quality == "hd" {
+//                                if (item["bitrate"] as! NSString).integerValue > highestBitrate {
+//                                    highestBitrate = (item["bitrate"] as! NSString).integerValue
+//                                    format = item
+//                                }
+//                            } else if self.quality == "sd" {
+//                                if (item["bitrate"] as! NSString).integerValue < highestBitrate {
+//                                    highestBitrate = (item["bitrate"] as! NSString).integerValue
+//                                    format = item
+//                                }
+//                            }
+//                        }
+//                    } else {format = aacFormats[0]}
+//                    self.streamUrl = format["url"] as! String
+//                    self.fileType = "m4a"
+//                }
+//
+//                if UserDefaults.standard.bool(forKey: settingsKeys.proxyContent) {
+//                    // modify streamURL to use instance proxying
+//                    let host = (URL(string: self.streamUrl)?.host)! as String
+//                    self.streamUrl = self.streamUrl.replacingOccurrences(of: host, with: UserDefaults.standard.string(forKey: settingsKeys.instanceUrl) ?? Constants.defaultInstance)
+//                }
+//
+//                self.progressBar.setRelativeWidth(0.65, withAdjustment: 0)
+//                UserDefaults.standard.set(self.streamUrl, forKey: hls.url)
+//
+//                self.statusLabel.setText("Ready.")
+//                self.showMovieFade(movie: self.movie)
+//                self.movieLoading.setHidden(true)
+//                self.movieLoading.stopAnimating()
+//                self.progressBar.setHidden(true)
+//
+//            case .failure(_):
+//                self.statusLabel.setText("Error getting data")
+//                self.movieLoading.stopAnimating()
+//                self.movieLoading.setImageNamed("error")
+//                self.progressBar.setHidden(true)
+//            }
+//        }.downloadProgress { progress in
+//            self.progressBar.setRelativeWidth(progress.fractionCompleted, withAdjustment: 0)
+//        }
     }
 }
 
